5cefba12b99348b61084a6de4c9b917342e7a23d
ruangsanadindonesia-art <ruangsanadindonesia@gmail.com>
2026-09-24 20:33:29 +0700

    feat: register membership pages

--- src/client/pages.ts
+++ src/client/pages.ts
@@ -9,11 +9,13 @@ import Admin from "./pages/Admin";
 import AdminChecklistNew from "./pages/AdminChecklistNew";
 import AdminChecklists from "./pages/AdminChecklists";
 import AdminRoles from "./pages/AdminRoles";
+import AdminUsers from "./pages/AdminUsers";
 import Checklists from "./pages/Checklists";
 import ChecklistRun from "./pages/ChecklistRun";
 import Dashboard from "./pages/Dashboard";
 import ForgotPassword from "./pages/ForgotPassword";
 import History from "./pages/History";
+import InvitationAccept from "./pages/InvitationAccept";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import OrganizationNew from "./pages/OrganizationNew";
@@ -31,10 +33,12 @@ export const pages: Record<string, PageModule> = {
 	"./pages/AdminChecklistNew.tsx": { default: AdminChecklistNew },
 	"./pages/AdminChecklists.tsx": { default: AdminChecklists },
 	"./pages/AdminRoles.tsx": { default: AdminRoles },
+	"./pages/AdminUsers.tsx": { default: AdminUsers },
 	"./pages/Checklists.tsx": { default: Checklists },
 	"./pages/ChecklistRun.tsx": { default: ChecklistRun },
 	"./pages/Dashboard.tsx": { default: Dashboard },
 	"./pages/History.tsx": { default: History },
+	"./pages/InvitationAccept.tsx": { default: InvitationAccept },
 	"./pages/ForgotPassword.tsx": { default: ForgotPassword },
 	"./pages/Login.tsx": { default: Login },
 	"./pages/NotFound.tsx": { default: NotFound },
